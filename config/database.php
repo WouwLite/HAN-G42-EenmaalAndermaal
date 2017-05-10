a1f24cf759a7a91290f04da4dc9ada16af7ae1d6
Marijn Besseling <njirambem@gmail.com>
2017-05-10 22:13:03 +0200

    fixed database connection

--- config/database.php
+++ config/database.php
@@ -1,13 +1,11 @@
 <!-- /config/database.php -->
 <?php
-
-$DB_NAME        = 'iproject42';
-$DB_USERNAME    = 'iproject42';
-$DB_PASSWD      = '7MqNNSxC';
-$DB_HOST        = 'mssql2.iproject.icaites.nl';
-
-try{
-    $connection = new PDO("dblib:host=$DB_HOST;dbname=$DB_NAME;", $DB_USERNAME, $DB_PASSWD);
-} catch(PDOException $e) {
-    die ( "Connection failed!" . $e->getMessage());
+$hostname = "mssql2.iproject.icasites.nl";
+$dbname = "iproject42";
+$username = "iproject42";
+$pw = "7MqNNSxC";
+try {
+    $pdo = new PDO ("sqlsrv:Server=$hostname;Database=$dbname;ConnectionPooling=0", "$username", "$pw");
+} catch (PDOException $e) {
+    die($e->getMessage());
 }
