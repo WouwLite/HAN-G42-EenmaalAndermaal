cd17c8b14e5b12099a2a1419496c2dc3f1910d6d
komcommy <joel@vdwouw.me>
2017-05-11 19:06:26 +0200

    Added login.php -> still fixing the code though. Building generic footer for template.

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -1,2 +1,62 @@
+<!-- /include/footer.inc.php -->
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/include/style.inc.php') ?>
+<link rel="stylesheet" href="../assets/css/footer.css">
+<footer>
+    <div class="footer" id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                    <h3>Klantenservice</h3>
+                    <ul>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                    <h3>FUCKED IT...</h3>
+                    <ul>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                    <h3>Over ons</h3>
+                    <ul>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                    <h3>Fuck me</h3>
+                    <ul>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3  col-md-3 col-sm-6 col-xs-12 ">
+                    <h3>Awesome</h3>
+                    <ul>
+                        <li>
+                            <div class="input-append newsletter-box text-center">
+                                <input type="text" class="full text-center" placeholder="Email ">
+                                <button class="btn  bg-gray" type="button"> Lorem ipsum <i class="fa fa-long-arrow-right"> </i> </button>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!--/.row-->
+        </div>
+        <!--/.container-->
+    </div>
+    <!--/.footer-->
+
+    <div class="footer-bottom">
+        <div class="container">
+            <p class="pull-left"> Copyright © Footer 2014. All right reserved. </p> // change
+            <div class="pull-right">
+                hello world, pull right!
+            </div>
+        </div>
+    </div>
+    <!--/.footer-bottom-->
+</footer>
+
 </body>
 </html>
