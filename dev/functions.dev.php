c0b8cd2e14524549a31b879344699f4d74643702
Marijn Besseling <njirambem@gmail.com>
2017-05-12 14:40:41 +0200

    moved getRealPOST function to seperate directory

--- dev/functions.dev.php
+++ dev/functions.dev.php
@@ -10,18 +10,4 @@ $testName       = 'Billy the Kid';
 $testBiedingNo  = '4';
 $testAdvertNo   = '2';
 
-
-// helper functions
-function getRealPOST()
-{
-    $pairs = explode("&", file_get_contents("php://input"));
-    $vars = array();
-    foreach ($pairs as $pair) {
-        $nv = explode("=", $pair);
-        $name = urldecode($nv[0]);
-        $value = urldecode($nv[1]);
-        $vars[$name] = $value;
-    }
-    return $vars;
-}
 ?>
