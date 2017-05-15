d05cb5ac480ac2cc58b85e6fb0de35939aeef309
Jurian <jurianbaldenhofer@live.nl>
2017-05-15 15:31:36 +0200

    FUlly almost working account page

--- views/merchant/API.php
+++ views/merchant/API.php
@@ -20,16 +20,13 @@ try {
         array(
             'amount' => $_SESSION['amount'],
             'description' => 'Upgrade naar verkoper',
-            'redirectUrl' => 'https://webshop.example.org/order/'.$_SESSION['orderNumber'].'/',
+            'redirectUrl' => 'http://iproject42.icasites.nl/views/account/index.php#/'.$_SESSION['orderNumber'].'/',
             'metadata' => array(
                 'order_id' => ''.$_SESSION['orderNumber'].'',
             )
         )
     );
 
-    /*
-    * Send the customer off to complete the payment.
-    */
     header("Location: " . $payment->getPaymentUrl());
     exit;
 } catch (Mollie_API_Exception $e) {
