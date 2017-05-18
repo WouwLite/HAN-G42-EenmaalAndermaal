90daea796bef4afd5c01930b39bbc5e2a6f76dcc
casparhendrik <casparhendrik12@gmail.com>
2017-05-18 15:37:57 +0200

    Merge branch 'master' of C:\Users\Caspar\PhpstormProjects\HAN-G42-EenmaalAndermaal with conflicts.

--- views/account/changeAd.php
+++ views/account/changeAd.php
@@ -119,193 +119,200 @@ function updateProductData()
         $updateAdInfo = $pdo->prepare($stmt);
         $updateAdInfo->execute(array($title, $description, (float)$startprice, (int)$paymentmethod, $paymentinstruction,
             (int)$duration, $durationbeginDay, $durationbeginTime,
-            (float)$shippingCosts, $shippingInstructions, $durationendDay, $durationendTime, (int)$categorieName, $productid));
+            (float)$shippingCosts, $shippingInstructions, $durationendDay, $durationendTime, (int)$categorieName, (int)$productid));
         //header('location: ../account/index.php');
         print_r($updateAdInfo->errorInfo());
     }
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Advertentie aanmaken</title>
-</head>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Advertentie aanmaken</title>
+    </head>
 
-<body>
+    <body>
 
-<div class="container-float">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="<?=$app_url?>">Thuis</a></li>
-        <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
-        <li class="breadcrumb-item active">Advertentie wijzigen</li>
-    </ol>
-</div>
-<div class="col-10">
-    <a href="<?=$app_url?>" class="btn btn-info btn-lg" role="button" aria-pressed="true">Terug</a>
-</div>
+    <div class="container-float">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="<?= $app_url ?>">Thuis</a></li>
+            <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+            <li class="breadcrumb-item active">Advertentie wijzigen</li>
+        </ol>
+    </div>
+    <div class="col-10">
+        <a href="<?= $app_url ?>" class="btn btn-info btn-lg" role="button" aria-pressed="true">Terug</a>
+    </div>
 
-<div class="container main-part">
-    <form action="#" method="post">
-        <div class="form-group row">
-            <label class="col-2 col-form-label"></label>
-            <div class="col-8">
-                <h1 class="product-title-page">Wijzig advertentie</h1>
+    <div class="container main-part">
+        <form action="#" method="post">
+            <div class="form-group row">
+                <label class="col-2 col-form-label"></label>
+                <div class="col-8">
+                    <h1 class="product-title-page">Wijzig
+                        advertentie <?php echo 'Het advertentie nummer is: ' . $_GET['id']; ?></h1>
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-2 col-form-label">Categorie*</label>
-            <div class="col-10">
-                <select class="form-control" id="Categories" name="Categories">
-                    <?php
-                    $stmt = $pdo->prepare("SELECT * FROM Categories");
-                    $stmt->execute();
-                    $data = $stmt->fetchAll();
-                    foreach ($data as $row) { ?>
-<!--                        <option>--><?php //echo $row['Name']?><!--</option>-->
-                        <option <?php print(($dataAd['Categories'] == $row['ID'])?"selected":"")?> value="<?=$row['ID']?>"><?php echo $row['Name']?></option>
+            <div class="form-group row">
+                <label class="col-2 col-form-label">Categorie*</label>
+                <div class="col-10">
+                    <select class="form-control" id="Categories" name="Categories">
                         <?php
-                    }
-                    ?>
-                </select>
+                        $stmt = $pdo->prepare("SELECT * FROM Categories");
+                        $stmt->execute();
+                        $data = $stmt->fetchAll();
+                        foreach ($data as $row) { ?>
+                            <!--                        <option>--><?php //echo $row['Name']?><!--</option>-->
+                            <option <?php print(($dataAd['Categories'] == $row['ID']) ? "selected" : "") ?>
+                                    value="<?= $row['ID'] ?>"><?php echo $row['Name'] ?></option>
+                            <?php
+                        }
+                        ?>
+                    </select>
+                </div>
             </div>
-        </div>
-        <div <?php print((!empty($errors['title'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
-            <label for="title" class="col-2 col-form-label">Titel:*</label>
-            <div class="col-10">
+            <div <?php print((!empty($errors['title'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
+                <label for="title" class="col-2 col-form-label">Titel:*</label>
+                <div class="col-10">
 
-                <input id="title" type="text" id="title" name="title" class="form-control" placeholder="Titel:" value="<?php echo $dataAd['title']; ?>"
-                       autofocus>
-                <div class="form-control-feedback"><?php global $errors;
-                    echo $errors['title'] ?></div>
+                    <input id="title" type="text" id="title" name="title" class="form-control" placeholder="Titel:"
+                           value="<?php echo $dataAd['title']; ?>"
+                           autofocus>
+                    <div class="form-control-feedback"><?php global $errors;
+                        echo $errors['title'] ?></div>
+                </div>
             </div>
-        </div>
 
-        <input type="hidden" value="<?php echo $_GET['id'];?>" name="productid" id="productid">
+            <input type="hidden" value="<?php echo $_GET['id']; ?>" name="productid">
 
-        <div <?php print((!empty($errors['description'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
-            <label class="col-2 col-form-label">Beschrijving:*</label>
-            <div class="col-10">
+            <div <?php print((!empty($errors['description'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
+                <label class="col-2 col-form-label">Beschrijving:*</label>
+                <div class="col-10">
             <textarea class="form-control" rows="4" name="description"
-              placeholder="Plaats hier een beschrijving van uw product"><?php echo $dataAd['description']; ?></textarea>
-                <div class="form-control-feedback"><?php global $errors;
-                    echo $errors['description'] ?></div>
+                      placeholder="Plaats hier een beschrijving van uw product"><?php echo $dataAd['description']; ?></textarea>
+                    <div class="form-control-feedback"><?php global $errors;
+                        echo $errors['description'] ?></div>
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-2 col-form-label">Upload tot 4 fotos:</label>
-            <div class="col-10">
-                <input type="file" name="foto1" id="foto2" class="form-control">
-                <input type="file" name="foto2" id="foto1" class="form-control">
-                <input type="file" name="foto3" id="foto3" class="form-control">
-                <input type="file" name="foto4" id="foto4" class="form-control">
+            <div class="form-group row">
+                <label class="col-2 col-form-label">Upload tot 4 fotos:</label>
+                <div class="col-10">
+                    <input type="file" name="foto1" id="foto2" class="form-control">
+                    <input type="file" name="foto2" id="foto1" class="form-control">
+                    <input type="file" name="foto3" id="foto3" class="form-control">
+                    <input type="file" name="foto4" id="foto4" class="form-control">
+                </div>
             </div>
-        </div>
-        <div <?php print((!empty($errors['startprice'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
-            <label class="col-2 col-form-label">Prijs*</label>
-            <div class="input-inline col-10">
-                <div class="form-check">
+            <div <?php print((!empty($errors['startprice'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
+                <label class="col-2 col-form-label">Prijs*</label>
+                <div class="input-inline col-10">
+                    <div class="form-check">
         <span class="inline-input">
         <input onclick="check()" type="radio" id="radio1" class="minimum-bid-price" value="<?php echo $dataAd['shippingcosts'];?>">
                         <label for="minimum-bid-price">Start bieden vanaf:</label>
-                </div>
-                </span>
+                    </div>
+                    </span>
 
-                <div class="form-check">
+                    <div class="form-check">
                 <span class="inline-input">
             <input onclick="uncheck()" type="radio" id="123" class="minimum-bid-price">
             <label>Geen minimale prijs</label>
                 </span>
+                    </div>
+                    <input id="minimum-bid-price" placeholder="€ 0,00" name="startprice"
+                           value="<?php echo $dataAd['startprice']; ?>" type="number" class="form-control"
+                           disabled>
+                    <div class="form-control-feedback"><?php global $errors;
+                        echo $errors['startprice'] ?></div>
                 </div>
-                <input id="minimum-bid-price" placeholder="€ 0,00" name="startprice" value="<?php echo $dataAd['startprice']; ?>" type="number" class="form-control"
-                       disabled>
-                <div class="form-control-feedback"><?php global $errors;
-                    echo $errors['startprice'] ?></div>
             </div>
-        </div>
 
-        <div <?php print((!empty($errors['paymentmethod'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
-            <label class="col-2 col-form-label">Betaal methode:*</label>
-            <div class="col-10">
-                <select class="form-control" name="paymentmethod" id="payment-method">
-                    <option <?php print(($dataAd['paymentmethodNumber'] == 1)?"selected":"")?> name="Creditcard">Creditcard</option>
-                    <option <?php print(($dataAd['paymentmethodNumber'] == 2)?"selected":"")?> name="Bankgiro">Bankgiro</option>
-                </select>
-                <div class="form-control-feedback"><?php global $errors;
-                    echo $errors['paymentmethod'] ?></div>
+            <div <?php print((!empty($errors['paymentmethod'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
+                <label class="col-2 col-form-label">Betaal methode:*</label>
+                <div class="col-10">
+                    <select class="form-control" name="paymentmethod" id="payment-method">
+                        <option <?php print(($dataAd['paymentmethodNumber'] == 1) ? "selected" : "") ?>
+                                name="Creditcard">Creditcard
+                        </option>
+                        <option <?php print(($dataAd['paymentmethodNumber'] == 2) ? "selected" : "") ?> name="Bankgiro">
+                            Bankgiro
+                        </option>
+                    </select>
+                    <div class="form-control-feedback"><?php global $errors;
+                        echo $errors['paymentmethod'] ?></div>
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-2 col-form-label">Verzendkosten:</label>
-            <div class="col-10">
-                <input id="minimum-bid-price" placeholder="€ 0,00" name="shippingcosts" type="number" step="0.01" value="<?php echo $dataAd['shippingcosts'];?>"
-                       class="form-control">
+            <div class="form-group row">
+                <label class="col-2 col-form-label">Verzendkosten:</label>
+                <div class="col-10">
+                    <input id="minimum-bid-price" placeholder="€ 0,00" name="shippingcosts" type="number" step="0.01"
+                           value="<?php echo $dataAd['shippingcosts']; ?>"
+                           class="form-control">
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-2 col-form-label">Betaal instructie</label>
-            <div class="col-10">
+            <div class="form-group row">
+                <label class="col-2 col-form-label">Betaal instructie</label>
+                <div class="col-10">
         <textarea class="form-control" rows="4"
                   placeholder="Plaats hier betaal instructie" name="paymentinstruction"><?php echo $dataAd['paymentinstruction']; ?></textarea>
-            </div>
-            <label class="col-2 col-form-label">Verzend instructie</label>
-            <div class="col-10">
+                </div>
+                <label class="col-2 col-form-label">Verzend instructie</label>
+                <div class="col-10">
             <textarea class="form-control" rows="4"
                       placeholder="Plaats hier uw verzend instructie" name="shippinginstruction"><?php echo $dataAd['shippinginstructions']; ?></textarea>
+                </div>
             </div>
-        </div>
 
 
-        <div <?php print((!empty($errors['duration'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
-            <label for="daysforsayle" class="col-2 col-form-label">Aantal dagen biedtijd:*</label>
-            <div class="col-10">
-                <select class="form-control" id="duration" name="duration" id="daysforsayle">
-                    <option <?php print(($dataAd['duration'] == 1)?"selected":"")?>>1</option>
-                    <option <?php print(($dataAd['duration'] == 3)?"selected":"")?>>3</option>
-                    <option <?php print(($dataAd['duration'] == 5)?"selected":"")?>>5</option>
-                    <option <?php print(($dataAd['duration'] == 7)?"selected":"")?>>7</option>
-                </select>
-                <div class="form-control-feedback"><?php global $errors;
-                    echo $errors['duration'] ?></div>
+            <div <?php print((!empty($errors['duration'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
+                <label for="daysforsayle" class="col-2 col-form-label">Aantal dagen biedtijd:*</label>
+                <div class="col-10">
+                    <select class="form-control" id="duration" name="duration" id="daysforsayle">
+                        <option <?php print(($dataAd['duration'] == 1) ? "selected" : "") ?>>1</option>
+                        <option <?php print(($dataAd['duration'] == 3) ? "selected" : "") ?>>3</option>
+                        <option <?php print(($dataAd['duration'] == 5) ? "selected" : "") ?>>5</option>
+                        <option <?php print(($dataAd['duration'] == 7) ? "selected" : "") ?>>7</option>
+                    </select>
+                    <div class="form-control-feedback"><?php global $errors;
+                        echo $errors['duration'] ?></div>
+                </div>
             </div>
-        </div>
 
-        <div class="form-group row">
-            <label class="col-2 col-form-label"></label>
-            <div class="form-check col-10">
-                <input type="checkbox" required>
-                Ik ga akkoord met de algemene voorwaarden
+            <div class="form-group row">
+                <label class="col-2 col-form-label"></label>
+                <div class="form-check col-10">
+                    <input type="checkbox" required>
+                    Ik ga akkoord met de algemene voorwaarden
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <div class="col-10">
-                <button class="btn btn-success btn-lg" type="submit" name="final-submit">Doorgaan</button>
+            <div class="form-group row">
+                <div class="col-10">
+                    <button class="btn btn-success btn-lg" type="submit" name="final-submit">Doorgaan</button>
+                </div>
             </div>
-        </div>
-    </form>
-</div>
+        </form>
+    </div>
 
 
+    </body>
 
-</body>
+    <script>
+        function check() {
+            document.getElementById("minimum-bid-price").disabled = false;
+            document.getElementById("123").checked = false;
+        }
+        function uncheck() {
+            document.getElementById("minimum-bid-price").disabled = true;
+            document.getElementById("radio1").checked = false;
+            document.getElementById("minimum-bid-price").value = "€ 0,00";
+        }
+    </script>
 
-<script>
-    function check() {
-        document.getElementById("minimum-bid-price").disabled = false;
-        document.getElementById("123").checked = false;
-    }
-    function uncheck() {
-        document.getElementById("minimum-bid-price").disabled = true;
-        document.getElementById("radio1").checked = false;
-        document.getElementById("minimum-bid-price").value = "€ 0,00";
-    }
-</script>
-
-</html>
+    </html>
 
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . '/include/sidebar.inc.php');
 include($_SERVER['DOCUMENT_ROOT'] . '/include/footer.inc.php');
 include($_SERVER['DOCUMENT_ROOT'] . '/include/delete-modal.php');
-
 
