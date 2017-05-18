2991c4ff5da16223721969683a6b4915a8de3349
Marijn Besseling <njirambem@gmail.com>
2017-05-18 09:39:46 +0200

    adding some buttons to login page

--- views/account/login.php
+++ views/account/login.php
@@ -8,15 +8,16 @@ $pw = "7MqNNSxC";      //Password
 $pdo = new PDO ("sqlsrv:Server=$hostname;Database=$dbname;ConnectionPooling=0", "$username", "$pw");
 
 
-
-function checkEmptyFields(){
+function checkEmptyFields()
+{
     global $errors;
     global $vars;
-    $errors['username']    = ($vars['username'] == "")    ?  "vul je gebruikersnaam in aub.":'';
-    $errors['password']    = ($vars['password'] == "")    ?  "vul je wachtwoord in aub.":'';
+    $errors['username'] = ($vars['username'] == "") ? "vul je gebruikersnaam in aub." : '';
+    $errors['password'] = ($vars['password'] == "") ? "vul je wachtwoord in aub." : '';
 }
 
-function getRealPOST() {
+function getRealPOST()
+{
     $pairs = explode("&", file_get_contents("php://input"));
     $vars = array();
     foreach ($pairs as $pair) {
@@ -32,28 +33,26 @@ function getRealPOST() {
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST')  {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $vars = getRealPost();
     checkEmptyFields();
-    if(isset($vars['username']) && isset($vars['password'])){
+    if (isset($vars['username']) && isset($vars['password'])) {
         $username = $vars['username'];
         $stmt = $pdo->prepare("SELECT password FROM Users WHERE username = ?");
         $stmt->execute([$vars['username']]);
         $data = $stmt->fetch();
 
-        if($data){
+        if ($data) {
             $password_ok = password_verify($vars['password'], $data['password']);
-            if($password_ok){
+            if ($password_ok) {
                 session_start();
                 $_SESSION['username'] = $username;
                 header('location: index.php');
-            }
-            else {
+            } else {
                 $errors['password'] = "Onjuist wachtwoord";
                 $errors['username'] = " ";
             }
-        }
-        else {
+        } else {
             $errors['password'] = " ";
             $errors['username'] = "Onbekend gebruikersnaam";
         }
@@ -65,43 +64,53 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')  {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title><?=$title?></title>
+    <title><?= $title ?></title>
     <link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/assets/css/login-register.css">
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/include/style.inc.php'); ?>
 </head>
+<body>
+<a href="<?= $app_url ?>" class="btn btn-danger" role="button" aria-pressed="true"
+   style="margin-left: 5px;margin-top: 5px;">Terug naar thuis</a>
 <div>
-    <div>
-        <br><br>
-        <center><img src="/storage/images/logo/logo-ea-groot-licht.png" style="max-height: 70px" alt="EenmaalAndermaal Logo"></center>
-        <br><br>
-    </div>
-    <div class="container">
+    <br><br>
+    <center><img src="/storage/images/logo/logo-ea-groot-licht.png" style="max-height: 70px"
+                 alt="EenmaalAndermaal Logo"></center>
+    <br><br>
+</div>
+<div class="container">
+    <form class="form-horizontal sign-in-form" action="#" method="POST">
+        <div class="alert alert-info" role="alert">
+            Heb je nog geen account?<br> <a href="<?= $app_url ?>/views/account/register.php">klik dan hier om te
+                registreren</a>
+        </div>
+        <div <?php global $errors;
+        print((!empty($errors['username'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
+            <div class="input-group inputform row">
+                <span class="input-group-addon fa fa-user"></span>
+                <input type="text" id="username" name="username" class="form-control" placeholder="Gebruikersnaam"
+                       autofocus>
+            </div>
+            <div class="form-control-feedback"><?php global $errors;
+                echo $errors['username'] ?></div>
+        </div>
 
-        <form class="form-horizontal sign-in-form" action="#" method="POST">
-            <div class="form-group row <?php global $errors; print((!empty($errors['username']))?'has-danger"':'"'); ?>>
-        <div class="input-group inputform row">
-            <span class="input-group-addon fa fa-user"></span>
-            <input type="text" id="username" name="username" class="form-control" placeholder="Gebruikersnaam" autofocus>
-    </div>
-    <div class="form-control-feedback"><?php global $errors; echo $errors['username']?></div>
-</div>
+        <div <?php global $errors;
+        print((!empty($errors['password'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
+            <div class="input-group inputform row">
+                <span class="input-group-addon fa fa-lock" id="basicaddon1"></span>
+                <input type="password" id="password" name="password" class="form-control" placeholder="Wachtwoord">
+            </div>
+            <div class="form-control-feedback"><?php global $errors;
+                echo $errors['password'] ?></div>
+        </div>
 
-<div class="form-group row <?php global $errors; print((!empty($errors['password']))?'has-danger"':'"'); ?>>
-<!--        <label for="name" class="col-4 col-form-label"></label>-->
-<div class="input-group inputform row" >
-    <span class="input-group-addon fa fa-lock" id="basicaddon1"></span>
-    <input type="password" id="password" name="password" class="form-control" placeholder="Wachtwoord">
-</div>
-<div class="form-control-feedback"><?php global $errors; echo $errors['password']?></div>
-</div>
-
-<div class="form-group row inputform" style="margin:auto;">
-    <label class="col-sm-4 control-label"></label>
-    <div>
-        <button type="submit" class="btn btn-success btn-block">Login</button>
-    </div>
-</div>
-</form>
+        <div class="form-group row inputform" style="margin:auto;">
+            <label class="col-sm-4 control-label"></label>
+            <div>
+                <button type="submit" class="btn btn-success btn-block">Login</button>
+            </div>
+        </div>
+    </form>
 </div>
 
 <footer class="footer">
