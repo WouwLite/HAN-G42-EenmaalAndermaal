d46fba943d202e7bb9585626a31918b6d0cf8edf
komcommy <joel@vdwouw.me>
2017-05-24 12:20:56 +0200

    Totaal niet stuk gemaakt. xxxjes

--- views/account/login.php
+++ views/account/login.php
@@ -66,52 +66,59 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <title><?= $title ?></title>
-    <link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/assets/css/login-register.css">
+    <link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/assets/css/register.css">
+<!--    <link rel="stylesheet" href="--><?php //$_SERVER['DOCUMENT_ROOT'] ?><!--/assets/css/login-register.css">-->
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/include/style.inc.php'); ?>
 </head>
 <body>
-<a href="<?= $app_url ?>" class="btn btn-danger" role="button" aria-pressed="true"
-   style="margin-left: 5px;margin-top: 5px;">Terug naar thuis</a>
-<div>
-    <br><br>
-    <center><img src="/storage/images/logo/logo-ea-groot-licht.png" style="max-height: 70px"
-                 alt="EenmaalAndermaal Logo"></center>
-    <br><br>
-</div>
-<div class="container">
-    <form class="form-horizontal sign-in-form" action="#" method="POST">
-        <div class="alert alert-info" role="alert">
-            Heb je nog geen account?<br> <a href="<?= $app_url ?>/views/account/register.php">klik dan hier om te
-                registreren</a>
-        </div>
-        <div <?php global $errors;
-        print((!empty($errors['username'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
-            <div class="input-group inputform row">
-                <span class="input-group-addon fa fa-user"></span>
-                <input type="text" id="username" name="username" class="form-control" placeholder="Gebruikersnaam"
-                       autofocus>
-            </div>
-            <div class="form-control-feedback"><?php global $errors;
-                echo $errors['username'] ?></div>
-        </div>
 
-        <div <?php global $errors;
-        print((!empty($errors['password'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
-            <div class="input-group inputform row">
-                <span class="input-group-addon fa fa-lock" id="basicaddon1"></span>
-                <input type="password" id="password" name="password" class="form-control" placeholder="Wachtwoord">
-            </div>
-            <div class="form-control-feedback"><?php global $errors;
-                echo $errors['password'] ?></div>
+<div class="bg-overlay">
+    <div class="container col-md-8 col-xs-6 jumbotron" style="background: rgba(236, 240, 241, 0.9);">
+        Ok, de quote (") zit ergens verstopt in de bg-overlay of jumbotrol
+        <a href="<?= $app_url ?>" class="btn btn-danger" role="button" aria-pressed="true"
+           style="margin-left: 5px;margin-top: 5px;">Terug naar thuis</a>
+        <div>
+            <br><br>
+            <center><img src="/storage/images/logo/logo-ea-groot-donker.png" style="max-height: 70px"
+                         alt="EenmaalAndermaal Logo"></center>
+            <br><br>
         </div>
+        <div class="container">
+            <form class="form-horizontal sign-in-form" action="#" method="POST">
+                <div class="alert alert-info" role="alert">
+                    Heb je nog geen account?<br> <a href="<?= $app_url ?>/views/account/register.php">klik dan hier om te
+                        registreren</a>
+                </div>
+                <div <?php global $errors;
+                print((!empty($errors['username'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
+                    <div class="input-group inputform">
+                        <span class="input-group-addon fa fa-user"></span>
+                        <input type="text" id="username" name="username" class="form-control" placeholder="Gebruikersnaam"
+                               autofocus>
+                    </div>
+                    <div class="form-control-feedback"><?php global $errors;
+                        echo $errors['username'] ?></div>
+                </div>
 
-        <div class="form-group row inputform" style="margin:auto;">
-            <label class="col-sm-4 control-label"></label>
-            <div>
-                <button type="submit" class="btn btn-success btn-block">Login</button>
-            </div>
+                <div <?php global $errors;
+                print((!empty($errors['password'])) ? 'class="form-group row has-danger"' : 'class="form-group row"'); ?>>
+                    <div class="input-group inputform">
+                        <span class="input-group-addon fa fa-lock" id="basicaddon1"></span>
+                        <input type="password" id="password" name="password" class="form-control" placeholder="Wachtwoord">
+                    </div>
+                    <div class="form-control-feedback"><?php global $errors;
+                        echo $errors['password'] ?></div>
+                </div>
+
+                <div class="form-group row inputform" style="margin:auto;">
+                    <label class="col-sm-4 control-label"></label>
+                    <div>
+                        <button type="submit" class="btn btn-success btn-block">Login</button>
+                    </div>
+                </div>
+            </form>
         </div>
-    </form>
+    </div>
 </div>
 
 <footer class="footer">
