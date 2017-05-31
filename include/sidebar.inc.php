722ad09a41183b4b8341e385c2b904872ccfd98c
Marijn Besseling <njirambem@gmail.com>
2017-05-31 13:19:51 +0200

    implementing category system

--- include/sidebar.inc.php
+++ include/sidebar.inc.php
@@ -11,7 +11,8 @@
     <ul>
         <li>
             <form>
-                <input class="form-control sm-2" type="search" id="search" name="Search" placeholder="Zoek naar veiling..."/>
+                <input class="form-control sm-2" type="search" id="search" name="Search"
+                       placeholder="Zoek naar veiling..."/>
             </form>
         </li>
 
@@ -29,7 +30,7 @@
 
         <?php
         global $user;
-        if($user['admin'] == 1){
+        if ($user['admin'] == 1) {
             print('<li><strong>Admin</strong></li>
             <li class="nav-item">
                     <a class="nav-link" href="' . $app_url . '/views/admin/adminpanel.php"><i class="fa fa-handshake-o" aria-hidden="true"></i> Alle advertenties</a>
@@ -49,37 +50,119 @@
         <li><span class="sidebar-span"></span></li>
         <li><strong>Account</strong></li>
         <li class="nav-item">
-            <a class="nav-link" href="<?=$app_url?>/views/account/index.php"><i class="fa fa-gavel" aria-hidden="true"></i> Mijn biedingen <span class="badge badge-default"><?=$testBiedingNo?></span></a>
+            <a class="nav-link" href="<?= $app_url ?>/views/account/index.php"><i class="fa fa-gavel"
+                                                                                  aria-hidden="true"></i> Mijn biedingen
+                <span class="badge badge-default"><?= $testBiedingNo ?></span></a>
         </li>
         <!-- Create IF statement. If user is merchant, show this link, else hide -->
         <li class="nav-item">
-            <a class="nav-link" href="<?=$app_url?>/views/account/index.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Mijn advertenties <span class="badge badge-default"><?=$testAdvertNo?></span></a>
+            <a class="nav-link" href="<?= $app_url ?>/views/account/index.php"><i class="fa fa-shopping-cart"
+                                                                                  aria-hidden="true"></i> Mijn
+                advertenties <span class="badge badge-default"><?= $testAdvertNo ?></span></a>
         </li>
         <li><span class="sidebar-span"></span></li>
         <li><strong>Rubrieken</strong></li>
+        <!--        --><?php
+        //        $catsql = <<<SQL
+        //        SELECT Name FROM [top level categories];
+        //SQL;
+        //
+        //        $stmt = $pdo->prepare($catsql);
+        //        $stmt->execute();
+        //        $categories = $stmt->fetchAll(PDO::FETCH_COLUMN);
+        //        if ($categories) {
+        //            foreach($categories as $category) {
+        //                echo "<li class='nav-item'>";
+        //                echo "<a class='nav-link' href='" . $app_url . "/views/categories/" . $category . "'>" . $category . "</a>";
+        //                echo "</li>";
+        //            }
+        //        }
+        //        ?>
         <?php
-        $catsql = <<<SQL
-        SELECT Name FROM [top level categories];
-SQL;
-
+        $catsql = "SELECT * FROM Categories";
         $stmt = $pdo->prepare($catsql);
         $stmt->execute();
-        $categories = $stmt->fetchAll(PDO::FETCH_COLUMN);
-        if ($categories) {
-            foreach($categories as $category) {
-                echo "<li class='nav-item'>";
-                echo "<a class='nav-link' href='" . $app_url . "/views/categories/" . $category . "'>" . $category . "</a>";
-                echo "</li>";
+        $rawcategories = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $keyed = array();
+        foreach ($rawcategories as &$value) {
+            $keyed[$value['ID']] = &$value;
+        }
+        unset($value);
+        $rawcategories = $keyed;
+        unset($keyed);
+
+        // tree it
+        $tree = array();
+        foreach ($rawcategories as &$value) {
+            if ($parent = $value['Parent'])
+                $rawcategories[$parent]['children'][] = &$value;
+            else
+                $tree[] = &$value;
+        }
+        unset($value);
+        $treecategories = $tree;
+        unset($tree);
+
+        //        var_dump($treecategories);
+
+        function printParentAndChildren($parent)
+        {
+//            var_dump($parent);
+            echo <<<HTML
+            <div class="accordion nav-item" id="accordion{$parent['ID']}">
+                <div class="accordion-group">
+                    <div class="accordion-heading">
+HTML;
+            if (array_key_exists('children', $parent)) {
+                echo <<<HTML
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion{$parent['ID']}" href="#collapse{$parent['ID']}children" >
+                            {$parent['Name']} <!--<span class="badge badge-pill badge-default" > 69</span > -->
+                        </a>
+HTML;
+            } else {
+                echo <<<HTML
+                <a href="index.php?cat={$parent['Name']}">{$parent['Name']}</a>
+HTML;
+
+            }
+            echo "</div>";
+            if (array_key_exists('children', $parent)) {
+                echo <<<HTML
+                    <div id="collapse{$parent['ID']}children" class="accordion-body collapse"  style="padding-left: 1em";>
+                        <div class="accordion-inner nav-item">
+                            <!-- Here we insert another nested accordion -->
+HTML;
+
+                foreach ($parent['children'] as $child) {
+                    printParentAndChildren($child);
+                    echo "</div>";
+                }
+                echo "</div>";
+                echo "</div>";
+                echo "</div>";
+            } else {
+                echo "</div>";
             }
         }
+
+        echo "<li class='nav-item'>";
+        foreach ($treecategories[0]['children'] as $toplevel) {
+            printParentAndChildren($toplevel);
+        }
+        echo "</li>";
+
         ?>
     </ul>
+</div>
 
-    <div id="sidebar-hamburger">
-        <span></span>
-        <span></span>
-        <span></span>
-    </div>
+</ul>
+
+<div id="sidebar-hamburger">
+    <span></span>
+    <span></span>
+    <span></span>
+</div>
 </div>
 
 <script>
